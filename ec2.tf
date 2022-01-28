5e49ac034367577e967f111606a6e38fb701eb01
SwapnilManohar <71309232+SwapnilManohar@users.noreply.github.com>
2022-01-28 18:13:08 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-new-instance"
+
+  ami                    = "ami-0a8b4cd432b1c3063"
+  instance_type          = "t2.micro"
+  key_name               = "Swapnil-Vpc"
+  
+  
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
